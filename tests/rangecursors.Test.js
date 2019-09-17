cf526ea78ffd8d37d4878cfec8e906dcc511fdbd
andrewdove1 <andrew.dove@ni.com>
2019-09-17 10:03:07 -0500

    Added test

--- tests/rangecursors.Test.js
+++ tests/rangecursors.Test.js
@@ -547,6 +547,10 @@ describe('Flot cursors', function () {
         });
     });
     describe('cursorupdates', function() {
+        beforeAll(function () {
+            loadDragSimulators();
+        });
+    
         it("should be called when a cursor is added", function() {
             plot = $.plot(placeholder, [sampledata], {});
 
@@ -625,6 +629,45 @@ describe('Flot cursors', function () {
             expect(spy).toHaveBeenCalled();
             expect(cursorXStart).toBeCloseTo(1, 8);
             expect(cursorXEnd).toBeCloseTo(1.15, 8);
+        });
+
+        it("should send drag events when the range cursor is moved", function() {
+            var spy = jasmine.createSpy('spy');
+            placeholder.on('cursordragstart', spy);
+            var spy1 = jasmine.createSpy('spy');
+            placeholder.on('cursordrag', spy1);
+            var spy2 = jasmine.createSpy('spy');
+            placeholder.on('cursordragend', spy2);
+            plot = $.plot("#placeholder", [sampledata], {
+                pan: {
+                    interactive: true
+                },
+                rangecursors: [
+                    {
+                        name: 'Blue cursor',
+                        color: 'blue',
+                        position: { relativeXStart: 0.5, relativeXEnd: 0.6 }
+                    }
+                ]
+            });
+            jasmine.clock().tick(20);
+    
+            var cursor = plot.getRangeCursors()[0];
+            var options = { mouseX: plot.getPlotOffset().left + plot.width() * 0.5,
+                mouseY: plot.getPlotOffset().top + plot.height() * 0.6 };
+            $('.flot-overlay').simulate("flotdragstart", options);
+            jasmine.clock().tick(20);
+    
+            var cursorX = plot.getPlotOffset().left + plot.width() * 0.5 + 20;
+            var cursorY = plot.getPlotOffset().top + plot.height() * 0.2;
+                var options = { mouseX: cursorX, mouseY: cursorY, dx: 13, dy: 5 };
+            $('.flot-overlay').simulate("flotdrag", options);
+
+            $('.flot-overlay').simulate("flotdragend", options);
+            jasmine.clock().tick(20);
+            expect(spy).toHaveBeenCalled();
+            expect(spy1).toHaveBeenCalled();
+            expect(spy2).toHaveBeenCalled();
         });
     });
 });
